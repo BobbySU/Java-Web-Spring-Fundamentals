9e729030e9a3db822b25e6912ccda9a87fc782dc
BobbySU <radio_20bg@yahoo.com>
2023-01-28 15:00:05 +0200

    Add Repository and BeanConfiguration

---
+++ Mobile/src/main/java/com/example/Mobile/repository/ModelRepository.java
@@ -0,0 +1,9 @@
+package com.example.Mobile.repository;
+
+import com.example.Mobile.models.entity.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepository extends JpaRepository<Model, Long> {
+}
---
+++ Mobile/src/main/java/com/example/Mobile/repository/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.Mobile.repository;
+
+import com.example.Mobile.models.entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+}
