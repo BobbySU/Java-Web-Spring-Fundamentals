19f0d8eb35ca980c392ccc2c79e9855a0ba46ec9
BobbySU <radio_20bg@yahoo.com>
2023-02-18 10:09:35 +0200

    Add login

--- Spring Fundamentals Exam Prep - 12 February 2021/shoppingList/src/main/java/com/exam/shoppingList/service/UserService.java
+++ Spring Fundamentals Exam Prep - 12 February 2021/shoppingList/src/main/java/com/exam/shoppingList/service/UserService.java
@@ -1,4 +1,9 @@
 package com.exam.shoppingList.service;
 
+import com.exam.shoppingList.models.service.UserServiceModel;
+
 public interface UserService {
+    void register(UserServiceModel userServiceModel);
+
+    UserServiceModel findByUserAndPassword(String username, String password);
 }
--- Spring Fundamentals Exam Prep - 12 February 2021/shoppingList/src/main/java/com/exam/shoppingList/service/impl/UserServiceImpl.java
+++ Spring Fundamentals Exam Prep - 12 February 2021/shoppingList/src/main/java/com/exam/shoppingList/service/impl/UserServiceImpl.java
@@ -1,15 +1,32 @@
 package com.exam.shoppingList.service.impl;
 
+import com.exam.shoppingList.models.entity.User;
+import com.exam.shoppingList.models.service.UserServiceModel;
+import com.exam.shoppingList.repository.UserRepository;
 import com.exam.shoppingList.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-//    private final UserService userService;
-//
-//    public UserServiceImpl(UserService userService) {
-//        this.userService = userService;
-//    }
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public void register(UserServiceModel userServiceModel) {
+        userRepository.save(modelMapper.map(userServiceModel, User.class));
+    }
+
+    @Override
+    public UserServiceModel findByUserAndPassword(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, password)
+                .map(user -> modelMapper.map(user, UserServiceModel.class))
+                .orElse(null);
+    }
 }
