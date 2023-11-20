2a73c3357bb1a61f78e2d6d749f173911f3deda3
BobbySU <radio_20bg@yahoo.com>
2023-11-21 00:45:08 +0200

    Edit AuthController Validation

--- battle-ships/src/main/java/com/example/battleships/web/AuthController.java
+++ battle-ships/src/main/java/com/example/battleships/web/AuthController.java
@@ -1,9 +1,14 @@
 package com.example.battleships.web;
 
+import com.example.battleships.models.dto.UserRegisterDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
@@ -15,11 +20,25 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String postRegister(){
+    public String postRegister(@Valid @ModelAttribute(name = "userRegisterDTO") UserRegisterDTO userRegisterDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute ("userRegisterDTO", userRegisterDTO)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterDTO",
+                            bindingResult);
+        }
+
         return "register";
     }
     @GetMapping("/login")
     public String getLogin(){
         return "login";
+    }
+
+    // Model attributes
+    @ModelAttribute(name = "userRegisterDTO")
+    public UserRegisterDTO userRegisterDTO(){
+        return new UserRegisterDTO();
     }
 }
